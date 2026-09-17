f840fb9b00ad312ddfffe4450fda71b333c569f1
henriqueballao <henriqueballao@gmail.com>
2026-09-17 17:01:09 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='viniswim-v21';
+const CACHE='viniswim-v22';
 const CORE=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./viniswim-logo.svg'];
 
 self.addEventListener('install',e=>{
