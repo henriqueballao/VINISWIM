766cb413eb7e45f399d1e26390221a5ce74ad05a
henriqueballao <henriqueballao@gmail.com>
2026-09-19 19:43:30 -0300

    Forca PWA a carregar status DNS DSQ DNF

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const BUILD='61';
-const CACHE='viniswim-v61';
+const BUILD='62';
+const CACHE='viniswim-v62';
 const CORE=['./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./viniswim-logo.svg'];
 
 self.addEventListener('install',e=>{
