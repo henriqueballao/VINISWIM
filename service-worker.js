4c041898294b1fb7deecf0ed215c653b2ae017e4
henriqueballao <henriqueballao@gmail.com>
2026-09-19 17:09:42 -0300

    Forca PWA Vini a carregar fonte e resultado oficial atual

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const BUILD='60';
-const CACHE='viniswim-v60';
+const BUILD='61';
+const CACHE='viniswim-v61';
 const CORE=['./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./viniswim-logo.svg'];
 
 self.addEventListener('install',e=>{
