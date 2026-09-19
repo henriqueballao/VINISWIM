6570f7d36fe816325d5eb5b2535d389cddb07ee9
henriqueballao <henriqueballao@gmail.com>
2026-09-19 17:02:01 -0300

    Forca PWA a usar refresh assincrono V36

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const BUILD='59';
-const CACHE='viniswim-v59';
+const BUILD='60';
+const CACHE='viniswim-v60';
 const CORE=['./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./viniswim-logo.svg'];
 
 self.addEventListener('install',e=>{
