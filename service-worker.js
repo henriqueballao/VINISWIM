57958ba89a32675a16dc00ee239bdbdbfafe5678
henriqueballao <henriqueballao@gmail.com>
2026-09-18 21:31:07 -0300

    VINISWIM V43 - align service worker build

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const BUILD='42';
-const CACHE='viniswim-v42';
+const BUILD='43';
+const CACHE='viniswim-v43';
 const CORE=['./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./viniswim-logo.svg'];
 
 self.addEventListener('install',e=>{
