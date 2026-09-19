a1f2deeb9643ba19c72fb9a8ec82ac0073092e37
henriqueballao <henriqueballao@gmail.com>
2026-09-19 20:39:45 -0300

    Forca PWA a carregar correcao de status Railway

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const BUILD='64';
-const CACHE='viniswim-v64';
+const BUILD='65';
+const CACHE='viniswim-v65';
 const CORE=['./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./viniswim-logo.svg'];
 
 self.addEventListener('install',e=>{
