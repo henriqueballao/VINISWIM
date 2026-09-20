3adc39ad151ade0039be3b64a75cd94de1b69cc4
henriqueballao <henriqueballao@gmail.com>
2026-09-20 20:54:18 -0300

    Lorenzo V78: atualiza cache

--- atletas/lorenzo/service-worker.js
+++ atletas/lorenzo/service-worker.js
@@ -1,4 +1,4 @@
-const BUILD='73';
+const BUILD='78';
 const PREFIX='viniswim-lorenzo-';
 const CACHE=PREFIX+'v72';
 const CORE=['./','./index.html','./manifest.webmanifest','../../icon-192.png','../../icon-512.png'];
