00defb1707d459228543ef07c0466a458653885d
henriqueballao <henriqueballao@gmail.com>
2026-09-20 20:34:30 -0300

    VINISWIM V77: atualiza cache do app Vini

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const BUILD='76';
-const CACHE='viniswim-v76';
+const BUILD='77';
+const CACHE='viniswim-v77';
 const CORE=['./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./viniswim-logo.svg'];
 
 self.addEventListener('install',e=>{
