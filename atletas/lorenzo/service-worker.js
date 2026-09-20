e6ceb979e208314162dc1e1c473b7cccd432c0c4
henriqueballao <henriqueballao@gmail.com>
2026-09-20 05:47:37 -0300

    Forca PWA a carregar textos de fonte oficial

--- atletas/lorenzo/service-worker.js
+++ atletas/lorenzo/service-worker.js
@@ -1,6 +1,6 @@
-const BUILD='53-LORENZO-14';
+const BUILD='53-LORENZO-15';
 const PREFIX='viniswim-lorenzo-';
-const CACHE=PREFIX+'v12';
+const CACHE=PREFIX+'v13';
 const CORE=['./','./index.html','./manifest.webmanifest','../../icon-192.png','../../icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k.startsWith(PREFIX)&&k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim();const list=await self.clients.matchAll({type:'window',includeUncontrolled:true});await Promise.all(list.map(c=>c.navigate(c.url).catch(()=>{})));})());});
