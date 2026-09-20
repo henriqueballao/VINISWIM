46eb6dd384a28fe2675d9a131d9b5a5991877503
henriqueballao <henriqueballao@gmail.com>
2026-09-20 18:05:48 -0300

    VINISWIM V73: atualiza cache dos PWAs

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const BUILD='72';
-const CACHE='viniswim-v72';
+const BUILD='73';
+const CACHE='viniswim-v73';
 const CORE=['./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./viniswim-logo.svg'];
 
 self.addEventListener('install',e=>{
