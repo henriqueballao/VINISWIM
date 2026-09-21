b7676faab63349f90a62caa715db8a40051f1181
henriqueballao <henriqueballao@gmail.com>
2026-09-20 21:08:34 -0300

    André V80: atualiza cache

--- atletas/andre/service-worker.js
+++ atletas/andre/service-worker.js
@@ -1,4 +1,4 @@
-const BUILD='79';
+const BUILD='80';
 const PREFIX='viniswim-andre-';
 const CACHE=PREFIX+'v72';
 const CORE=['./','./index.html','./manifest.webmanifest','../../icon-192.png','../../icon-512.png'];
