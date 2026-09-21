1e362fefa37bc4e420bed51f0ca0c516ae8eed08
henriqueballao <henriqueballao@gmail.com>
2026-09-20 21:08:26 -0300

    Vini V80: atualiza cache

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const BUILD='79';
-const CACHE='viniswim-v79';
+const BUILD='80';
+const CACHE='viniswim-v80';
 const CORE=['./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./viniswim-logo.svg'];
 
 self.addEventListener('install',e=>{
