082f29c755315a88b5c93ba2cafaadcd269afe27
henriqueballao <henriqueballao@gmail.com>
2026-09-25 15:20:50 -0300

    Clean temporary VINISWIM boot marker

--- commercial/apps/web/src/main.tsx
+++ commercial/apps/web/src/main.tsx
@@ -3,5 +3,4 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>)
-;(window as any).__viniswimBooted=true
 if('serviceWorker'in navigator)window.addEventListener('load',()=>navigator.serviceWorker.register(import.meta.env.BASE_URL+'sw.js').catch(()=>{}))
